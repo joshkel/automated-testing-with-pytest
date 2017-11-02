603e98d631f07d2ecead0827accc6ed52eb098f7
Josh Kelley <joshkel@gmail.com>
2017-11-02 00:38:56 -0400

    More workon presentation

--- demo/fib/fib.py
+++ demo/fib/fib.py
@@ -7,3 +7,9 @@ def fib(n):
         return 1
     else:
         return fib(n - 1) + fib(n - 2)
+
+def closed_form_fib(n):
+    # Courtesy of Wolfram Alpha
+    sqrt_5 = 5 ** 0.5
+    phi = (1 + sqrt_5) / 2
+    return round(phi ** n / sqrt_5)
---
+++ demo/fib/test_fib.py
@@ -0,0 +1,15 @@
+from fib import fib
+import pytest
+
+def test_fib():
+    assert fib(0) == 0
+    assert fib(1) == 1
+    assert fib(3) == 2
+    assert fib(4) == 3
+    assert fib(5) == 5
+
+def test_negative_fib():
+    pass
+
+def test_big_fib():
+    assert fib(30) == 832040
